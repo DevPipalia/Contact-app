99660738f76ab09d75f8c2da2afbe2caa5be668b
Dev Pipalia <devpipalia003@gmail.com>
2023-06-05 18:59:10 +0530

    Initialized app with firebase configuration

---
+++ src/firebase-config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAHNk31IJsWzd8UjVi0kEtoOKIGlKhbSCM",
+  authDomain: "contact-form-65de1.firebaseapp.com",
+  projectId: "contact-form-65de1",
+  storageBucket: "contact-form-65de1.appspot.com",
+  messagingSenderId: "843088015526",
+  appId: "1:843088015526:web:ce781df4695205a87ec467",
+  measurementId: "G-9X7B64VTDN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
