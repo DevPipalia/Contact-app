27fb9e27a570112157a4859abaf1405451f5f6c5
Dev Pipalia <devpipalia003@gmail.com>
2023-06-16 23:02:52 +0530

    undo usecontext authentication

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import Home from "./Pages/Home";
 import Add_user from "./Pages/Add_user";
 import LoginPage from "./Pages/Login";
 import SignUpPage from "./Pages/SignUp";
-import { UserAuthContextProvider } from "./UserAuthContext";
+
 
 function App() {
   return (
     <div className="App">
      
     <BrowserRouter>
-    <UserAuthContextProvider>     <Routes>
+    <Routes>
         <Route path="/" element={<LoginPage/>}/>
         <Route path="/signup" element={<SignUpPage/>}/>
         <Route path="/home" element={<Home/>}/>
         <Route path="/add_user" element={<Add_user/>}/>
       </Routes>
-      </UserAuthContextProvider>
+      
   
     </BrowserRouter>
     </div>
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,30 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import { useUserAuth } from "../UserAuthContext";
+import { auth } from "../firebase-config";
+
 function SignUpPage(){
     const [email,setEmail]=useState("")
     const [password,setPassword]=useState("")
-    const[error,setError]=useState("");
-    const {signup}=useUserAuth();
-    const handleSubmit= async(e)=>{
-        e.prevent.default()
-        setError("")
-        try {
-            await signup(email,password)
-        } catch (error) {
-
-            setError(error.message);
-        }
-
-    };
+   
 
 
 
     return(
         <>
             <h1>Sign Up</h1>
-            <form onSubmit={handleSubmit}>
+            <form >
             <input type="email" placeholder="Email.." onChange={(e)=>setEmail(e.target.value)}/>
             <input type="password" placeholder="Password..." onChange={(e)=>setPassword(e.target.value)}/>
             <input type="submit" value="Sign Up"/>
