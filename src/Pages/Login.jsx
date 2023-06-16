05dc408cf84c23e05f53652b4281d52a5cda3aeb
Dev Pipalia <devpipalia003@gmail.com>
2023-06-16 23:25:56 +0530

    login successful

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase-config";
+import { useNavigate } from "react-router-dom";
 
 function LoginPage(){
+
+    const navigate=useNavigate();
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            navigate('/home')
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+    }
+
+
     return(
         <>
+
         <h1>Login</h1>
-        <form>
-            <input type="email" placeholder="Email.."/>
-            <input type="password" placeholder="Password..."/>
+        <form onSubmit={handleSubmit}>
+            <input type="email" placeholder="Email.."  onChange={(e)=>setEmail(e.target.value)}/>
+            <input type="password" placeholder="Password..."  onChange={(e)=>setPassword(e.target.value)}/>
             <input type="submit" value="Login"/>
             <button>Sign in with Google</button>
             <p>Dont have an account <Link to="/signup">signup</Link> </p>
